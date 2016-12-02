d39bc816f242951b2a4e18c900d3001837bcc951
Raghu Betina <raghu@firstdraft.com>
2016-12-02 00:50:10 +0000

    Direct association between professors and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ApplicationRecord
   # Direct associations
 
+  belongs_to :professor
+
   has_many   :class_members,
              :foreign_key => "class_id",
              :dependent => :destroy
