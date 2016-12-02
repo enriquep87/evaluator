f7f4dd82bf5999ad87facba73fdc396b0f76c267
Raghu Betina <raghu@firstdraft.com>
2016-12-02 00:50:10 +0000

    Direct association between courses and class_members

--- app/models/class_member.rb
+++ app/models/class_member.rb
@@ -1,6 +1,9 @@
 class ClassMember < ApplicationRecord
   # Direct associations
 
+  belongs_to :class,
+             :class_name => "Course"
+
   belongs_to :student
 
   # Indirect associations
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ApplicationRecord
   # Direct associations
 
+  has_many   :class_members,
+             :foreign_key => "class_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
